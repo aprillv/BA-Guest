6976a602222c5cb100ec5dc83ee3334d507df4b3
roberto ramirez <robertoramirez@robertos-Mac-mini.local>
2014-01-11 09:20:00 +0800

    Initial Commit

---
+++ BA-Guest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BA-Guest
+//
+//  Created by roberto ramirez on 1/11/14.
+//  Copyright (c) 2014 lovetthomes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
